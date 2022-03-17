856d978ec678279415eeb62f2d8e5568ca1ddf2d
KenwoodFox <kenwood364@gmail.com>
2022-03-17 12:00:22 -0400

    Format black

--- tests/test_application.py
+++ tests/test_application.py
@@ -12,26 +12,25 @@ def app():
 
 @pytest.fixture
 def kvt():
-    return KeyValueTable('/tmp/test_table.db')
+    return KeyValueTable("/tmp/test_table.db")
 
 
 class TestApplication(object):
-
     def test_versioning(self, app):
         # App version must be present
         assert len(app.version) > 1
 
     def test_config_mountpoint(self, app):
         # Mountpoint must point where we expect
-        assert os.path.isdir('/config') is True
+        assert os.path.isdir("/config") is True
 
     def test_key_value_table(self, kvt):
-        kvt['bot_token'] = '0101u72636462615517'
-        kvt['email_listen'] = 'doooooooooooooot'
-        kvt['email_password'] = 'password'
+        kvt["bot_token"] = "0101u72636462615517"
+        kvt["email_listen"] = "doooooooooooooot"
+        kvt["email_password"] = "password"
 
         assert len(kvt.items()) == 3
 
-        assert kvt['bot_token'] == '0101u72636462615517'
+        assert kvt["bot_token"] == "0101u72636462615517"
 
         kvt.close()
